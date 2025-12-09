3d808adb04775ba09112de648024f3277a070162
Robohrriday <robohrriday.8@gmail.com>
2025-12-09 14:21:13 +0530

    updated for my system

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 ROOT=$(pwd)
+export CC=/usr/bin/gcc
+export CXX=/usr/bin/g++
+export CUDAHOSTCXX=/usr/bin/g++
+
 cd /kaolin && pip install -e .
 cd ${ROOT}/BundleSDF/mycuda && rm -rf build *egg* && pip install -e .
 cd ${ROOT}/BundleSDF/BundleTrack && rm -rf build && mkdir build && cd build && cmake .. && make -j11
